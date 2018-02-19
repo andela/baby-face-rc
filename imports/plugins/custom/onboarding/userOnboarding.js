67814a44985a02e66e962ddc84aa090dff9e93d5
Justin Nebo <nebojustin@gmail.com>
2018-02-19 13:55:13 +0100

    feature: add user and admin onboarding to the application
    [Starts #154621042]

---
+++ imports/plugins/custom/onboarding/userOnboarding.js
@@ -0,0 +1,68 @@
+const userOnboarding = [
+  {
+    intro: `<h4>Welcome to Reaction Commerce</h4>
+    <hr />
+    <p>Experience a quick tour of how to navigation through the interface of the application</p>`
+  },
+  {
+    element: "#cart-container",
+    intro: `<h4>Shopping Cart</h4>
+    <hr />
+    <p>Click here to proceed to checkout all the products already selected for purchase</p>`
+  },
+  {
+    element: "#accounts",
+    intro: `<h4>Accounts</h4>
+    <hr />
+    <p>Click here to register, login or access other account options</p>`
+  },
+  {
+    element: "#static-pages",
+    intro: `<h4>Pages</h4>
+    <hr />
+    <p>Click here to access static pages for more information</p>`
+  },
+  {
+    element: "#currencies",
+    intro: `<h4>Currency</h4>
+    <hr />
+    <p>Click here to select your preferred currency</p>`
+  },
+  {
+    element: "#languages",
+    intro: `<h4>Languages</h4>
+    <hr />
+    <p>Click here to select your preferred language</p>`
+  },
+  {
+    element: "#search",
+    intro: `<h4>Search</h4>
+    <hr />
+    <p>Click here to search for a product</p>`
+  },
+  {
+    element: ".tag-group",
+    intro: `<h4>Tag group</h4>
+    <hr />
+    <p>Click here to view all the products in each tag</p>`
+  },
+  {
+    element: ".brand",
+    intro: `<h4>Brand</h4>
+    <hr />
+    <p>Click here to go back to the home page</p>`
+  },
+  {
+    element: "#take-tour",
+    intro: `<h4>Take Tour</h4>
+    <hr />
+    <p>Click here to retake the onboarding anytime</p>`
+  },
+  {
+    intro: `<h4>End of onboarding</h4>
+    <hr />
+    <p>Enjoy your shopping experience</p>`
+  }
+];
+
+export default userOnboarding;
